5d3d4189a06881d232e1e4ce7f4f37dcb66a493c
orej <j.orechovas@pralo.eu>
2018-05-14 17:59:14 +0300

    [IMP] event_private: deleted unnecessary files, manifest fix

--- event_private/__manifest__.py
+++ event_private/__manifest__.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2016-TODAY Linserv Aktiebolag, Sweden (<http://www.linserv.se>).
+#
+##############################################################################
 {
-    'name': 'Event private',
-    'summary': 'Make event private',
-    'description': '',
-    'author': "Linserv AB",
-    'website': "http://www.linserv.se",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Events',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'website', 'event', 'website_event'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        # 'static/src/data/event_private.xml'
+    'name': "Event Private",
+    "version": "1.0",
+    "author": "Linserv AB",
+    "category": "Events",
+    "summary": "Makes private events",
+    "website": "www.linserv.se",
+    "contributors": [],
+    "license": "",
+    "depends": ['base', 'website_event'],
+    'description': """ 
+    
+        App makes private events
+        
+    """,
+    "demo": [],
+    "data": [
+        'static/src/xml/assets.xml',
+        'views/inherited_website_event.xml',
+        'views/inherited_event.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-    'installable': True,
-    'application': True,
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
 }
--- event_private/controllers/controller.py
+++ event_private/controllers/controller.py
@@ -3,9 +3,9 @@ from odoo import fields, http, _
 from odoo.addons.website.models.website import slug
 from odoo.http import request
 
+
 class WebsiteEventController(http.Controller):
-    _inherit = 'event.event'
-    
+
     # Render custom short url.
     @http.route(['/event/p/<string:shorturl>'], type='http', auth="public", website=True)
     def event_register_shorturl(self, shorturl, **post):
@@ -21,11 +21,3 @@ class WebsiteEventController(http.Controller):
             'range': range,
         }
         return request.render("website_event.event_description_full", values)
-
-        # values = {
-        #     'event': event,
-        #     'main_object': event,
-        #     'range': range,
-        #     'registrable': event._is_event_registrable()
-        # }
-        # return request.render("website_event.event_description_full", values)
